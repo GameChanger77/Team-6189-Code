895a7caf6d940aada4e499abcdbda309871c4269
GameChanger77 <gamer4christ2004@gmail.com>
2021-02-01 14:17:03 -0600

    Implemented FTCLib to hopefully give us more functionality

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControlled.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControlled.java
@@ -1,79 +1,26 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-@TeleOp
 public class DriverControlled extends OpMode {
-
-    Robot robot = new Robot(telemetry);
-    Presets presets = new Presets(robot);
-
-    double x,y,r,p,spinPower,wobblePower,extenderPower,loadPower,wobblePos=0,wobbleInc=1000;
-    boolean isDpad_up = false, isDpad_down = false, isX = false;
+    Robot robot = new Robot();
+    double strafe, forward, turn, power;
 
     @Override
     public void init() {
         robot.init(hardwareMap);
-        robot.noEncoders();
-        robot.wobble.resetEncoders();
-        robot.wobble.useEncoders();
     }
 
     @Override
     public void loop() {
-        // toggles
-        if(gamepad2.dpad_up && !isDpad_up){  // dpad up
-            robot.wobble.toPosition();
-            wobblePos += wobbleInc;
-        }
-        isDpad_up = gamepad2.dpad_up;
-        if(gamepad2.dpad_down && !isDpad_down){ // dpad down
-            wobblePos -= wobbleInc;
-        }
-        isDpad_down = gamepad2.dpad_down;
-        if(gamepad1.x && !isX){  // x pressed
-            presets.runNextPreset();
-        }
-        isX = gamepad1.x;
+        if(gamepad1.right_trigger > .25)
+            power = gamepad1.right_trigger;
+        else
+            power = .25;
+        strafe = gamepad1.left_stick_x * power;
+        forward = gamepad1.left_stick_y * power;
+        turn = gamepad1.right_stick_x * power;
 
-        // movement
-        p = gamepad1.right_trigger;
-        x = gamepad1.left_stick_x;
-        y = -gamepad1.left_stick_y;
-        r = gamepad1.right_stick_x;
-        if(p < 0.25)
-            p = 0.25;
-
-        // secondary game pad controls
-        if (gamepad2.x) {
-            spinPower = .65;
-        } else if (gamepad2.y) {
-            spinPower = .75;
-        } else if(gamepad2.b) {
-            spinPower = .55;
-        } else{
-            spinPower = gamepad2.right_trigger;
-        }
-        wobblePower = -gamepad2.left_stick_y;
-        extenderPower = -gamepad2.left_stick_x;
-        loadPower = gamepad2.right_stick_y;
-
-        // set everything
-        robot.move(x,y,r,p);
-        robot.launcher.launch(spinPower);
-        robot.launcher.load(loadPower);
-        // robot.wobble.lift(wobblePower);
-        robot.wobble.raiseToPos(wobblePos, wobblePower);
-        robot.wobble.extend(extenderPower);
-        robot.wobble.grip(gamepad2.right_stick_x);
-        telemetry.addData("/> WOBBLE POS", robot.wobble.lifter.getCurrentPosition());
-    }
-
-
-
-    @Override
-    public void stop() {
-        robot.shutdown();
+        robot.move(strafe, forward, turn);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/presets/IMovements.java
+++
@@ -1,9 +0,0 @@
-package org.firstinspires.ftc.teamcode.presets;
-
-import org.firstinspires.ftc.teamcode.Robot;
-
-public interface IMovements {
-    void rotate();
-    void path();
-    void move();
-}
